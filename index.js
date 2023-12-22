dbef22c1469880f3850202f73ee796da6b994646
MaqaTech <ceferli.mehemmed@mail.ru>
2023-12-22 21:58:32 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var overlay = document.querySelector('.overlay')
+var loading = document.querySelector('.loader')
+
+setTimeout(function(){
+    overlay.style.display = 'none'
+    loading.style.display = 'none'
+},3000)
